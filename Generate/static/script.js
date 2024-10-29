d8bde13593bce7297c4ae94db521696aa817a6d5
Gustavo Jun Miyamoto Hassegawa <90336592+GustavoHassegawa@users.noreply.github.com>
2024-10-29 19:11:54 -0300

    Update script.js

--- Generate/static/script.js
+++ Generate/static/script.js
@@ -29,64 +29,136 @@ async function uploadCertificateInfo() {
 async function generateCertificate() {
     const name = document.getElementById("name").value;
     const horas = document.getElementById("horas").value;
+    const coursename = document.getElementById("coursename").value;
+    const template = document.getElementById("template").value;
 
     if (!name) {
         alert("Please enter a name.");
         return;
     }
 
-    await uploadCertificateInfo();
+    if(template == "Voluntário") {
+        await uploadCertificateInfo();
 
-    const certificateCanvas = document.getElementById("certificateCanvas");
-    const certificateContext = certificateCanvas.getContext("2d");
+        const certificateCanvas = document.getElementById("certificateCanvas");
+        const certificateContext = certificateCanvas.getContext("2d");
 
-    // Here we load the certificate template as an image
-    const templateImage = new Image();
-    templateImage.src = "/static/certificadotemplate.png"; // Use certificate template image here
+        // Here we load the certificate template as an image
+        const templateImage = new Image();
+        templateImage.src = "/static/CertificadosTemplate.png"; // Use certificate template image here
 
-    console.log("downloading certificate: ", generated_qr_code);
-    const generated_qr_code_img = new Image()
-    generated_qr_code_img.crossOrigin = "anonymous"
-    generated_qr_code_img.src = generated_qr_code;
+        console.log("downloading certificate: ", generated_qr_code);
+        const generated_qr_code_img = new Image()
+        generated_qr_code_img.crossOrigin = "anonymous"
+        generated_qr_code_img.src = generated_qr_code;
 
-    templateImage.onload = function () {
-        // Here we set canvas dimensions to match the template image
-        certificateCanvas.width = templateImage.width;
-        certificateCanvas.height = templateImage.height;
+        templateImage.onload = function () {
+            // Here we set canvas dimensions to match the template image
+            certificateCanvas.width = templateImage.width;
+            certificateCanvas.height = templateImage.height;
 
-        // Here we draw the template image on the canvas
-        certificateContext.drawImage(templateImage, 0, 0);
+            // Here we draw the template image on the canvas
+            certificateContext.drawImage(templateImage, 0, 0);
 
-        // Here we customize the certificate by adding the user's name
-        // Centraliza o nome
-        certificateContext.font = "bold 80px Arial";
-        certificateContext.fillStyle = "black";
-        const textWidth = certificateContext.measureText(name).width;
-        const x = (certificateCanvas.width - textWidth) / 2; // Centraliza o texto
-        const y = 1238; // Posição Y onde o nome será desenhado
+            // Here we customize the certificate by adding the user's name
+            // Centraliza o nome
+            certificateContext.font = "bold 80px Arial";
+            certificateContext.fillStyle = "black";
+            const textWidth = certificateContext.measureText(name).width;
+            const x = (certificateCanvas.width - textWidth) / 2; // Centraliza o texto
+            const y = 1198; // Posição Y onde o nome será desenhado
 
-        certificateContext.fillText(name,x ,y); // Adjust position here
+            certificateContext.fillText(name,x ,y); // Adjust position here
 
-        //Adicionar as horas ao certificado
-        certificateContext.font = "bold 70px Arial";
-        certificateContext.fillStyle = "black";
+            //Adicionar as horas ao certificado
+            certificateContext.font = " 80px Arial";
+            certificateContext.fillStyle = "black";
 
-        certificateContext.fillText(horas, 2720, 1430);
+            const textoTemplate = 'participou do IV WORKSHOP DAS ENGENHARIAS em São Bernardo do Campo/SP,';
+            const textWidthTemplate = certificateContext.measureText(textoTemplate).width;
+            const xTemplate = (certificateCanvas.width - textWidthTemplate) / 2;
 
-        const additionalText = " horas"; // O texto que você quer adicionar
-        const additionalTextWidth = certificateContext.measureText(horas).width; // Largura do texto de horas
-        const textX = 2720 + additionalTextWidth; // Posição X para o texto adicional
+            const texto2 = `durante o período de 05 a 06 de Novembro de 2024, com carga horária de ${horas} horas.`;
+            const textWidth2 = certificateContext.measureText(texto2).width;
+            const x2 = (certificateCanvas.width - textWidth2) / 2;
 
-        certificateContext.fillText(additionalText, textX, 1430); // Desenha o texto adicional
-        
-        certificateContext.drawImage(generated_qr_code_img, 1445, 1434, 600, 600);
+            certificateContext.fillText(textoTemplate, xTemplate, 1350)
+            certificateContext.fillText(texto2, x2, 1430);
 
-        // Here we convert canvas to data URL (PNG)
-        const certificateDataURL = certificateCanvas.toDataURL("image/png");
+            certificateContext.drawImage(generated_qr_code_img, 1445, 1434, 600, 600);
 
-        // Here we set the download link href
-        const certificateLink = document.getElementById("downloadLink");
-        certificateLink.href = certificateDataURL;
-        certificateLink.style.display = "block";
-    };
+            // Here we convert canvas to data URL (PNG)
+            const certificateDataURL = certificateCanvas.toDataURL("image/png");
+
+            // Here we set the download link href
+            const certificateLink = document.getElementById("downloadLink");
+            certificateLink.href = certificateDataURL;
+            certificateLink.style.display = "block";
+        };
+    } else {
+
+        await uploadCertificateInfo();
+
+        const certificateCanvas = document.getElementById("certificateCanvas");
+        const certificateContext = certificateCanvas.getContext("2d");
+
+        // Here we load the certificate template as an image
+        const templateImage = new Image();
+        templateImage.src = "/static/CertificadosTemplate.png"; // Use certificate template image here
+
+        console.log("downloading certificate: ", generated_qr_code);
+        const generated_qr_code_img = new Image()
+        generated_qr_code_img.crossOrigin = "anonymous"
+        generated_qr_code_img.src = generated_qr_code;
+
+        templateImage.onload = function () {
+            // Here we set canvas dimensions to match the template image
+            certificateCanvas.width = templateImage.width;
+            certificateCanvas.height = templateImage.height;
+
+            // Here we draw the template image on the canvas
+            certificateContext.drawImage(templateImage, 0, 0);
+
+            // Here we customize the certificate by adding the user's name
+            // Centraliza o nome
+            certificateContext.font = "bold 80px Arial";
+            certificateContext.fillStyle = "black";
+            const textWidth = certificateContext.measureText(name).width;
+            const x = (certificateCanvas.width - textWidth) / 2; // Centraliza o texto
+            const y = 1158; // Posição Y onde o nome será desenhado
+
+            certificateContext.fillText(name,x ,y); // Adjust position here
+
+            //Adicionar as horas ao certificado
+            certificateContext.font = " 80px Arial";
+            certificateContext.fillStyle = "black";
+
+            const textoTemplate = `participou da ${coursename} como parte do IV WORKSHOP DAS ENGENHARIAS`;
+            const textWidthTemplate = certificateContext.measureText(textoTemplate).width;
+            const xTemplate = (certificateCanvas.width - textWidthTemplate) / 2;
+
+            const texto1 = "em São Bernardo do Campo/SP, durante o período de 05 a 06 de Novembro de 2024,";
+            const textWidth1 = certificateContext.measureText(texto1).width;
+            const x1 = (certificateCanvas.width - textWidth1) / 2;
+
+            const texto2 = `com carga horária de ${horas} horas.`;
+            const textWidth2 = certificateContext.measureText(texto2).width;
+            const x2 = (certificateCanvas.width - textWidth2) / 2;
+
+
+            certificateContext.fillText(textoTemplate, xTemplate, 1270);
+            certificateContext.fillText(texto1, x1, 1350);
+            certificateContext.fillText(texto2, x2, 1430);
+
+            certificateContext.drawImage(generated_qr_code_img, 1445, 1434, 600, 600);
+
+            // Here we convert canvas to data URL (PNG)
+            const certificateDataURL = certificateCanvas.toDataURL("image/png");
+
+            // Here we set the download link href
+            const certificateLink = document.getElementById("downloadLink");
+            certificateLink.href = certificateDataURL;
+            certificateLink.style.display = "block";
+        };
+    }
 }
