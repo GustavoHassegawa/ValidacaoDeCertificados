34344793a1057a5168f59b05623daab46d0c1f03
Gustavo Jun Miyamoto Hassegawa <90336592+GustavoHassegawa@users.noreply.github.com>
2024-11-13 17:42:54 -0300

    Update script.js

--- Generate/static/script.js
+++ Generate/static/script.js
@@ -95,6 +95,70 @@ async function generateCertificate() {
             certificateLink.href = certificateDataURL;
             certificateLink.style.display = "block";
         };
+    } else if (template == 'entidade') {
+        await uploadCertificateInfo();
+
+        const certificateCanvas = document.getElementById("certificateCanvas");
+        const certificateContext = certificateCanvas.getContext("2d");
+
+        // Here we load the certificate template as an image
+        const templateImage = new Image();
+        templateImage.src = "/static/CertificadoVersao2.png"; // Use certificate template image here
+
+        console.log("downloading certificate: ", generated_qr_code);
+        const generated_qr_code_img = new Image()
+        generated_qr_code_img.crossOrigin = "anonymous"
+        generated_qr_code_img.src = generated_qr_code;
+
+        templateImage.onload = function () {
+            // Here we set canvas dimensions to match the template image
+            certificateCanvas.width = templateImage.width;
+            certificateCanvas.height = templateImage.height;
+
+            // Here we draw the template image on the canvas
+            certificateContext.drawImage(templateImage, 0, 0);
+
+            // Here we customize the certificate by adding the user's name
+            // Centraliza o nome
+            certificateContext.font = "bold 80px Arial";
+            certificateContext.fillStyle = "black";
+            const textWidth = certificateContext.measureText(name).width;
+            const x = (certificateCanvas.width - textWidth) / 2; // Centraliza o texto
+            const y = 1158; // Posição Y onde o nome será desenhado
+
+            certificateContext.fillText(name,x ,y); // Adjust position here
+
+            //Adicionar as horas ao certificado
+            certificateContext.font = " 80px Arial";
+            certificateContext.fillStyle = "black";
+
+            const textoTemplate = `participou como representante da ${coursename} como parte do`;
+            const textWidthTemplate = certificateContext.measureText(textoTemplate).width;
+            const xTemplate = (certificateCanvas.width - textWidthTemplate) / 2;
+
+            const texto1 = "IV WORKSHOP DAS ENGENHARIAS em São Bernardo do Campo/SP, durante";
+            const textWidth1 = certificateContext.measureText(texto1).width;
+            const x1 = (certificateCanvas.width - textWidth1) / 2;
+
+            const texto2 = `o período de 05 a 06 de Novembro de 2024, com carga horária de ${horas} horas.`;
+            const textWidth2 = certificateContext.measureText(texto2).width;
+            const x2 = (certificateCanvas.width - textWidth2) / 2;
+
+
+            certificateContext.fillText(textoTemplate, xTemplate, 1270);
+            certificateContext.fillText(texto1, x1, 1350);
+            certificateContext.fillText(texto2, x2, 1430);
+
+            certificateContext.drawImage(generated_qr_code_img, 2645, 1434, 600, 600);
+
+            // Here we convert canvas to data URL (PNG)
+            const certificateDataURL = certificateCanvas.toDataURL("image/png");
+
+            // Here we set the download link href
+            const certificateLink = document.getElementById("downloadLink");
+            certificateLink.href = certificateDataURL;
+            certificateLink.style.display = "block";
+        };
     } else {
 
         await uploadCertificateInfo();
@@ -160,5 +224,5 @@ async function generateCertificate() {
             certificateLink.href = certificateDataURL;
             certificateLink.style.display = "block";
         };
-    }
+    } 
 }
